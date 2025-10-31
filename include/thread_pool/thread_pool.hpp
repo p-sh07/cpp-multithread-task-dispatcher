93d1edbfa37d2cae670d814b67dce1bc125c9777
Pavel Sh <p.sh07@ya.ru>
2025-10-31 19:39:31 +0300

    [add] thread pool

--- include/thread_pool/thread_pool.hpp
+++ include/thread_pool/thread_pool.hpp
@@ -1,9 +1,25 @@
 #pragma once
 
+#include "queue/priority_queue.hpp"
+
+#include <functional>
+#include <mutex>
+#include <thread>
+
 namespace dispatcher::thread_pool {
+using queue::PriorityQueue;
 
 class ThreadPool {
-  // здесь ваш код
+public:
+    explicit ThreadPool(std::shared_ptr<PriorityQueue> queue, size_t num_threads);
+
+    ~ThreadPool();
+
+private:
+    void Worker() const;
+
+    std::shared_ptr<PriorityQueue> task_queue_;
+    std::vector<std::jthread> workers_;
 };
 
 } // namespace dispatcher::thread_pool
--- src/thread_pool/thread_pool.cpp
+++ src/thread_pool/thread_pool.cpp
@@ -4,4 +4,32 @@ namespace dispatcher::thread_pool {
 
 // здесь ваш код
 
+ThreadPool::ThreadPool(std::shared_ptr<PriorityQueue> queue, size_t num_threads) {
+    workers_.reserve(num_threads);
+    generate_n(
+        std::back_inserter(workers_),
+        num_threads,
+        [this] { return std::jthread(&ThreadPool::Worker, this); }
+    );
+    // for(auto i : vw::iota(0u, num_threads)) {
+    //     workers_.emplace_back(&ThreadPool::Worker, this);
+    // }
+}
+
+ThreadPool::~ThreadPool() {
+    task_queue_->shutdown();
+
+    for(auto& worker : workers_) {
+        if(worker.joinable()) {
+            worker.join();
+        }
+    }
+}
+
+void ThreadPool::Worker() const {
+    //Do tasks while not std::nullopt from Q
+    while(auto task = task_queue_->pop()) {
+        (*task)(); //perform task
+    }
+}
 } // namespace dispatcher::thread_pool
